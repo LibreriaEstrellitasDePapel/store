98c09822d728be0acc48acca8e241ef6fc6b286c
Librería estrellitas de papel <libreria.estrellitasdepapel@gmail.com>
2025-12-30 15:12:05 -0300

    Add files via upload

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import catalog
+from .views import catalog, add_to_cart, view_cart
 
 urlpatterns = [
     path('', catalog, name='catalog'),
+    path('add/<int:book_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', view_cart, name='view_cart'),
 ]
